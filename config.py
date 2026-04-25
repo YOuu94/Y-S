a89f804d41e31bea77735d36a4bd4d36d44603ea
Y|⁹⁴|S <yz.shaheen0@gmail.com>
2026-04-25 06:03:47 +0300

    Create config.py with bot and database settings
    Add configuration settings for bot and support channels.

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+
+# بيانات البوت والمطور
+BOT_TOKEN = os.getenv('BOT_TOKEN') # سيتم وضعه في Railway
+OWNER_ID = 8511180085
+OWNER_USERNAME = "Y9_S4"
+OWNER_NAME = "Y ⁹|⁴ S"
+
+# بيانات الدعم
+SUPPORT_CHANNEL_USER = "fi1_oo"
+SUPPORT_CHANNEL_NAME = "SUPPORT | Y|⁹⁴|S"
+
+# روابط الانستغرام
+INSTA_1 = "https://www.instagram.com/1.0_v_?igsh=YzljYTk1ODg3Zg=="
+INSTA_2 = "https://www.instagram.com/1.0_c_?igsh=YzljYTk1ODg3Zg=="
+
+# رابط قاعدة البيانات (Railway يوفر هذا تلقائياً)
+DATABASE_URL = os.getenv('DATABASE_URL')
